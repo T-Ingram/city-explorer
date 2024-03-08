1eec0ec69c6509cee012e2d40f40f0c5a56481aa
Trevor Ingram <ingramtrevor10@gmail.com>
2024-03-07 19:04:15 -0800

    Rendered map

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const LocationInfo = ({ displayName, latitude, longitude }) => {
+  // Construct the URL for the static map image
+  const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${import.meta.env.VITE_API_KEY_LOCATIONIQ}&center=${latitude},${longitude}&zoom=13&size=800x600&format=png`;
+
   return (
     <div className="card">
       <div className="card-body">
@@ -8,6 +11,8 @@ const LocationInfo = ({ displayName, latitude, longitude }) => {
         <p className="card-text">City: {displayName}</p>
         <p className="card-text">Latitude: {latitude}</p>
         <p className="card-text">Longitude: {longitude}</p>
+        {/* Display the map image */}
+        <img src={mapUrl} alt="Map" className="img-fluid" />
       </div>
     </div>
   );
